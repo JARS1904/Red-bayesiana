c578e6c08282d762a38359c76fc54530e1459ad6
Jose Armando <armandorodriguezseg19@gmail.com>
2023-07-21 16:24:56 -0600

    Este es el primer commit del proyecto

---
+++ ejemplo_red_bayesiana.py
@@ -0,0 +1,38 @@
+#Use este en el documento
+from pgmpy.models import BayesianNetwork
+from pgmpy.factors.discrete import TabularCPD
+from pgmpy.inference import VariableElimination
+
+# Definir la estructura de la red
+model = BayesianNetwork([('Fiebre', 'Diagnóstico'), ('Congestión Nasal', 'Diagnóstico')])
+
+# Definir las distribuciones de probabilidad condicional (CDPs)
+cdp_fiebre = TabularCPD(variable='Fiebre', variable_card=2, values=[[0.4], [0.6]])
+cdp_congestion = TabularCPD(variable='Congestión Nasal', variable_card=2, values=[[0.7], [0.3]])
+
+cdp_diagnostico = TabularCPD(variable='Diagnóstico', variable_card=2,
+                            values=[[0.8, 0.2, 0.9, 0.1], [0.2, 0.8, 0.1, 0.9]],
+                            evidence=['Fiebre', 'Congestión Nasal'],
+                            evidence_card=[2, 2])
+
+# Asignar las CDPs al modelo
+model.add_cpds(cdp_fiebre, cdp_congestion, cdp_diagnostico)
+
+# Verificar si la red bayesiana es válida
+if model.check_model() == True:
+    print("El modelo es valido\n")
+else:
+    print("El modelo no es valido\n")
+
+# Hacer inferencia en la red bayesiana
+inference = VariableElimination(model)
+
+# Calcular la probabilidad de tener gripe dada una fiebre alta y congestión nasal presente
+print("Probabilidad de tener gripe dada una fiebre alta y congestión nasal presente")
+prob_gripe = inference.query(variables=['Diagnóstico'], evidence={'Fiebre': 1, 'Congestión Nasal': 1})
+print(prob_gripe, "\n")
+
+# Calcular la probabilidad de tener resfriado dada una fiebre normal y congestión nasal ausente
+print("Probabilidad de tener resfriado dada una fiebre normal y congestión nasal ausente")
+prob_resfriado = inference.query(variables=['Diagnóstico'], evidence={'Fiebre': 0, 'Congestión Nasal': 0})
+print(prob_resfriado)
