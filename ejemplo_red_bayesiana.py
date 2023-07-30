1cefc2910e06410d26095ca4b79670ba6207671a
Jose Armando <armandorodriguezseg19@gmail.com>
2023-07-29 20:28:13 -0600

    Se colocaron solo las librerias al inicio, se anadio el manejo de excepciones y el resultado se guarda en un archivo de texto

--- ejemplo_red_bayesiana.py
+++ ejemplo_red_bayesiana.py
@@ -1,9 +1,11 @@
-#Use este en el documento
+#Este archivo lo estoy usando para seguir aprendiendo Python y Git/Github
 import warnings
-warnings.filterwarnings("ignore", category=DeprecationWarning)
 from pgmpy.models import BayesianNetwork
 from pgmpy.factors.discrete import TabularCPD
 from pgmpy.inference import VariableElimination
+
+# Ignora la advertencia de Deprecado(poaibles funciones que en un futuro se eliminaran de las librerias)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 # Definir la estructura de la red
 model = BayesianNetwork([('Fiebre', 'Diagnóstico'), ('Congestión Nasal', 'Diagnóstico')])
@@ -27,12 +29,31 @@ if model.check_model() == True:
 # Hacer inferencia en la red bayesiana
 inference = VariableElimination(model)
 
-# Calcular la probabilidad de tener gripe dada una fiebre alta y congestión nasal presente
-print("Probabilidad de tener gripe dada una fiebre alta y congestión nasal presente")
-prob_gripe = inference.query(variables=['Diagnóstico'], evidence={'Fiebre': 1, 'Congestión Nasal': 1})
-print(prob_gripe, "\n")
+# Manejo de excepciones
+try:
+    # Se crea un archivo .txt
+    archivo = open("RedBayesiana.txt", "w", encoding="utf8")
 
-# Calcular la probabilidad de tener resfriado dada una fiebre normal y congestión nasal ausente
-print("Probabilidad de tener resfriado dada una fiebre normal y congestión nasal ausente")
-prob_resfriado = inference.query(variables=['Diagnóstico'], evidence={'Fiebre': 0, 'Congestión Nasal': 0})
-print(prob_resfriado)
+    # Calcular la probabilidad de tener gripe dada una fiebre alta y congestión nasal presente
+    print("Probabilidad de tener gripe dada una fiebre alta y congestión nasal presente")
+    prob_gripe = inference.query(variables=['Diagnóstico'], evidence={'Fiebre': 1, 'Congestión Nasal': 1})
+    print(prob_gripe, "\n")
+    # prob_gripe se vuelve un str para pasarlo a un archivo .txt
+    prob_gripe_str = str(prob_gripe)
+    archivo.write("Probabilidad de tener gripe dada una fiebre alta y congestión nasal presente\n")
+    archivo.write(prob_gripe_str)
+
+    # Calcular la probabilidad de tener resfriado dada una fiebre normal y congestión nasal ausente
+    print("Probabilidad de tener resfriado dada una fiebre normal y congestión nasal ausente")
+    prob_resfriado = inference.query(variables=['Diagnóstico'], evidence={'Fiebre': 0, 'Congestión Nasal': 0})
+    print(prob_resfriado)
+    # prob_resfriado se vuelve un str para pasarlo a un archivo .txt
+    prob_resfriado_str = str(prob_resfriado)
+    archivo.write("\n\nProbabilidad de tener resfriado dada una fiebre normal y congestión nasal ausente\n")
+    archivo.write(prob_resfriado_str)
+
+except Exception as e:
+    print(f"Ocurrio un error: {e}, {type(e)}")
+
+finally:
+    archivo.close()
